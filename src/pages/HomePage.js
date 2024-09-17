1fdc0908b01211608911bf686c89bb857298fbbe
meSajied <sajiedshahyousuf@gmail.com>
2024-09-17 14:51:39 +0600

    Added donation feature

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,41 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { BarChart } from "../component/BarChart";
-import costData from "../data/CostData.json";
+import {DailyFinancialStats} from "../component/DailyFinancialStats";
 
 function HomePage() {
-  //const [costData, setCostData] = useState([]);
-  const [cData, setCData] = useState({
-    labels: [],
-    datasets: [{
-      label: "",
-      data: [],
-    }]
-  });
-
-  useEffect(() => {
-    if (costData && costData.length > 0) {
-      setCData({
-        labels: costData.map((item) => item.date),
-        datasets: [{
-          label: "Gain",
-          data: costData.map((item) => item.gain),
-          backgroundColor: "rgba(253, 173, 0, 0.6)",
-        },
-          {
-            label: "Expense",
-            data: costData.map((item) => item.expense),
-            backgroundColor: "rgba(0, 0, 0, 0.6)"
-          }]
-      });
-    }
-  }, []);
-
 
   return (
-      <div className='p-4 w-full max-w-4xl mx-auto'>
-        <div className='bg-white p-4 shadow rounded'>
-          <BarChart data={cData} />
-        </div>
+      <div>
+        <DailyFinancialStats />
       </div>
   );
 }
--- src/pages/donation.js
+++ src/pages/donation.js
@@ -1,10 +1,37 @@
 import React from "react"
+import {DailyFinancialStats} from "../component/DailyFinancialStats";
 function Donation() {
   return(
-      <div>
-        <h1>Donation</h1>
+      <div className='flex-col items-center space-y-6'>
+        <div className='space-y-4'>
+          <div className='text-3xl text-center'>
+            Donation till now
+          </div>
+
+          <div className='font-mono text-center text-4xl'>
+            8000
+          </div>
+        </div>
+
+        <form className='p-4 flex justify-center space-x-4 items-center'
+            onSubmit={handleSubmit}>
+          <div className='text-xl space-x-2'>
+            <label htmlFor='amount' >Amount:</label>
+            <input className='border-2 rounded border-black'
+                type='text' name='amount' value='undefined'/>
+          </div>
+
+          <button className='border-2 rounded p-2 bg-black
+              text-white text-xl' type='submit'>Donate</button>
+        </form>
+
+        <DailyFinancialStats />
       </div>
-  )
+  );
+
+  function handleSubmit() {
+
+  }
 }
 
-export { Donation };
+export {Donation};
